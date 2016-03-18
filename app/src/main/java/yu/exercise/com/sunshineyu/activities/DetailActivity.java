dadf7248ff4ef5e436aa0502a1d9bd30ca0ea3b1
ChristianneYu <yuchristianne@gmail.com>
2016-03-18 19:03:06 +0800

    Added the sync and data folder, utility java, drawables

--- app/src/main/java/yu/exercise/com/sunshineyu/activities/DetailActivity.java
+++ app/src/main/java/yu/exercise/com/sunshineyu/activities/DetailActivity.java
@@ -1,25 +1,42 @@
 package yu.exercise.com.sunshineyu.activities;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ShareActionProvider;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 
 import yu.exercise.com.sunshineyu.R;
+import yu.exercise.com.sunshineyu.data.WeatherContract.WeatherEntry;
 
 public class DetailActivity extends AppCompatActivity {
-
+//
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detail);
+        setContentView(R .layout.activity_detail);
+
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
     }
+
+
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -38,4 +55,61 @@ public class DetailActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+
+    public static abstract class DetailFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
+
+        private static final String LOG_TAG = DetailFragment.class.getSimpleName();
+
+        private static final String FORECAST_SHARE_HASHTAG = " #SunshineApp";
+
+        private ShareActionProvider mShareActionProvider;
+        private String mForecast;
+
+        private static final int DETAIL_LOADER = 0;
+
+        private static final String[] FORECAST_COLUMNS = {
+                WeatherEntry.TABLE_NAME + "." + WeatherEntry._ID,
+                WeatherEntry.COLUMN_DATE,
+                WeatherEntry.COLUMN_SHORT_DESC,
+                WeatherEntry.COLUMN_MAX_TEMP,
+                WeatherEntry.COLUMN_MIN_TEMP,
+        };
+
+        private static final int COL_WEATHER_ID = 0;
+                private static final int COL_WEATHER_DATE = 1;
+                private static final int COL_WEATHER_DESC = 2;
+                private static final int COL_WEATHER_MAX_TEMP = 3;
+                private static final int COL_WEATHER_MIN_TEMP = 4;
+    }
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_detail, container, false);
+    }
+
+
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        inflater.inflate(R.menu.menu_fragment_detail, menu);
+
+        // Retrieve the share menu item
+        MenuItem menuItem = menu.findItem(R.id.action_share);
+        ShareActionProvider mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
+
+//        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+//        shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+//        shareIntent.setType("text/plain");
+//        shareIntent.putExtra(Intent.EXTRA_TEXT,shareIntent.putExtra(Intent.EXTRA_TEXT, menu + FORECAST_SHARE_HASHTAG);
+//        return shareIntent;
+
+
+
+    }
+
+
+
+
+
 }
